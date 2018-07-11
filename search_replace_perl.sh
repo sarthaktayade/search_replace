a0b289612c89d3e678cc89fc35781fb13bbe128b
Sarthak Tayade <sarthak@Sarthaks-MacBook-Pro-2.local>
2018-07-12 02:22:04 +0530

    Made sure that search_replace_perl.sh works for multiline text as well.
    - Also added an example call for search_replace_perl.sh

--- search_replace_perl.sh
+++ search_replace_perl.sh
@@ -6,6 +6,17 @@ SEARCH_REGEX=$1
 REPLACE_REGEX=$2
 
 find $ROOT_DIR -name "*.swift" | while read f; do
-	perl -pi.back -e "s/$SEARCH_REGEX/$REPLACE_REGEX/" $f		
+	perl -0pi.back -e "s/$SEARCH_REGEX/$REPLACE_REGEX/" $f		
 	echo "Completed edit $f"
 done
+
+# The following example searches for all callsites of myFun() and only renames the second argument label.
+# This basically means that indentation, variable names being passed etc are not affected at all callsites.
+# 
+#	Search - 	<anyobject>.myFun(param1: "param1",
+# 								param2: "param2") 
+#
+#	Replace - 	<anyobject>.myFun(param1: "param1",
+#								param3: ""param2)
+#
+# ./search_replace_perl.sh "\.myFun\(param1: (\.*[a-zA-Z0-9\"]*),(\.*[ \t\n]*)param2: (\.*[a-zA-Z0-9\"]*)" ".myFun\(param1: \$1,\$2param3: \$3"
